df39c28a55eb3439d6505129389d819c1a11ad76
liam-powers <95184359+liam-powers@users.noreply.github.com>
2024-07-21 12:31:32 -0500

    feat: client pulls from production URL

--- frontend/src/app/upright/UprightClientComponent.tsx
+++ frontend/src/app/upright/UprightClientComponent.tsx
@@ -1,176 +1,209 @@
 "use client";
-import { useState, useEffect } from 'react';
-import { UprightBassFilters } from '../components/FilterInterfaces';
-import UprightFilterButton from './UprightFilterButton';
-import { Input } from '@nextui-org/react';
-import PriceRangeComponent from './PriceRangeComponent';
-import 'rc-slider/assets/index.css';
-import UprightListingsGrid from './UprightListingsGrid';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { UprightBassFilters } from "../components/FilterInterfaces";
+import UprightFilterButton from "./UprightFilterButton";
+import { Input } from "@nextui-org/react";
+import PriceRangeComponent from "./PriceRangeComponent";
+import "rc-slider/assets/index.css";
+import UprightListingsGrid from "./UprightListingsGrid";
+import axios from "axios";
 
 const fetchListings = async (filters: UprightBassFilters) => {
-    const functionUrl = 'http://127.0.0.1:5001/bass-harbor-free/us-central1/fetchListings'; //TODO: change to production URL
-  
-    try {
-      const response = await axios({
-        method: 'post', // or 'get' if you prefer
-        url: functionUrl,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        data: {
-            priceRange: filters.priceRange,
-            keywords: filters.keywords,
-            carved: filters.carved,
-            hybrid: filters.hybrid,
-            plywood: filters.plywood
-        },
-      });
+  const functionUrl = "https://fetchlistings-jgzal2tqsa-uc.a.run.app"; // production URL
 
-      console.log("Fetch listings completed. ");
-      console.log("Response.data: ", response.data);
+  try {
+    const response = await axios({
+      method: "post", // or 'get' if you prefer
+      url: functionUrl,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        priceRange: filters.priceRange,
+        keywords: filters.keywords,
+        carved: filters.carved,
+        hybrid: filters.hybrid,
+        plywood: filters.plywood,
+      },
+    });
 
-      return response.data;
-    } catch (error) {
-      console.error('Error triggering scrape and add:', error);
-      throw error;
-    }
-}
+    console.log("Fetch listings completed. ");
+    console.log("Response.data: ", response.data);
+
+    return response.data;
+  } catch (error) {
+    console.error("Error triggering scrape and add:", error);
+    throw error;
+  }
+};
 
 interface UprightBassListing {
-    title?: string;
-    imgLink: string;
-    listingLink?: string;
-    location?: string;
-    saleStatus?: string;
-    price?: string;
-    year?: number;
-    maker?: string;
-    source: string;
+  title?: string;
+  imgLink: string;
+  listingLink?: string;
+  location?: string;
+  saleStatus?: string;
+  price?: string;
+  year?: number;
+  maker?: string;
+  source: string;
 }
 
-const renderKeywords = (keywords: String[], removeKeyword: (keyword: String) => void) => {
-    return (
-        <div className="flex gap-2 pt-4 flex-wrap" style={{ maxWidth: '300px' }}>
-            {keywords.map((keyword) => (
-                <button className="hover:line-through bg-pastel-red outline-black text-white font-bold py-2 px-4 rounded-lg"
-                    onClick={() => removeKeyword(keyword)}>{keyword}</button>
-            ))}
-        </div>
-    );
+const renderKeywords = (
+  keywords: String[],
+  removeKeyword: (keyword: String) => void,
+) => {
+  return (
+    <div className="flex gap-2 pt-4 flex-wrap" style={{ maxWidth: "300px" }}>
+      {keywords.map((keyword) => (
+        <button
+          className="hover:line-through bg-pastel-red outline-black text-white font-bold py-2 px-4 rounded-lg"
+          onClick={() => removeKeyword(keyword)}
+        >
+          {keyword}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 const UprightClientComponent = () => {
-    const [filters, setFilters] = useState<UprightBassFilters>({
-        keywords: [],
-        carved: false,
-        hybrid: false,
-        plywood: false,
-    });
+  const [filters, setFilters] = useState<UprightBassFilters>({
+    keywords: [],
+    carved: false,
+    hybrid: false,
+    plywood: false,
+  });
 
-    const [listings, setListings] = useState<UprightBassListing[]>([]);
+  const [listings, setListings] = useState<UprightBassListing[]>([]);
 
-    useEffect(() => {
-        const tryFetchListings = async () => {
-            try {
-                const fetchedListings = await fetchListings(filters);;// as UprightBassListing[];
-                console.log("Listings fetched: ", fetchedListings);
-                setListings(fetchedListings);
-            } catch (error) {
-                console.error('Error fetching listings:', error);
-            }
-        }
-
-        tryFetchListings();
-    }, [filters]);
-
-    const toggleFilter = (filterToToggle: keyof UprightBassFilters) => {
-        const updatedFilters = {
-            ...filters,
-            [filterToToggle]: !filters[filterToToggle]
-        };
-        setFilters(updatedFilters);
-        console.log("Successfully updated filters to: ", updatedFilters);
+  useEffect(() => {
+    const tryFetchListings = async () => {
+      try {
+        const fetchedListings = await fetchListings(filters); // as UprightBassListing[];
+        console.log("Listings fetched: ", fetchedListings);
+        setListings(fetchedListings);
+      } catch (error) {
+        console.error("Error fetching listings:", error);
+      }
     };
 
-    const removeKeyword = (keyword: String) => {
-        const updatedFilters = {
-            ...filters,
-            keywords: filters.keywords?.filter((kw) => kw !== keyword)
-        };
-        setFilters(updatedFilters);
-        console.log("Successfully updated filters to: ", updatedFilters);
-    }
+    tryFetchListings();
+  }, [filters]);
 
-    const [keyword, setKeyword] = useState("");
-
-    const handleKeywordAdd = (keyword: String) => {
-        if (keyword.trim() !== "") { // Check if the keyword is not empty
-            const updatedFilters = {
-                ...filters,
-                keywords: filters.keywords ? [...filters.keywords, keyword.trim()] : [keyword.trim()]
-            };
-            setFilters(updatedFilters);
-            setKeyword(""); // Clear keyword input after adding
-            console.log("Successfully updated filters to: ", updatedFilters);
-        }
+  const toggleFilter = (filterToToggle: keyof UprightBassFilters) => {
+    const updatedFilters = {
+      ...filters,
+      [filterToToggle]: !filters[filterToToggle],
     };
+    setFilters(updatedFilters);
+    console.log("Successfully updated filters to: ", updatedFilters);
+  };
 
-    if (!filters) {
-        return <div>Loading...</div>;
+  const removeKeyword = (keyword: String) => {
+    const updatedFilters = {
+      ...filters,
+      keywords: filters.keywords?.filter((kw) => kw !== keyword),
+    };
+    setFilters(updatedFilters);
+    console.log("Successfully updated filters to: ", updatedFilters);
+  };
+
+  const [keyword, setKeyword] = useState("");
+
+  const handleKeywordAdd = (keyword: String) => {
+    if (keyword.trim() !== "") {
+      // Check if the keyword is not empty
+      const updatedFilters = {
+        ...filters,
+        keywords: filters.keywords
+          ? [...filters.keywords, keyword.trim()]
+          : [keyword.trim()],
+      };
+      setFilters(updatedFilters);
+      setKeyword(""); // Clear keyword input after adding
+      console.log("Successfully updated filters to: ", updatedFilters);
     }
+  };
 
-    return (
-       <div className="pt-20">
-            <div className="flex flex-row w-full pl-20 pr-40 gap-40 justify-between">
-                <div className="sticky top-0 h-full flex flex-row pt-">
-                    <div className="min-w-[20rem]">
-                        <div className="flex flex-col items-center">
-                            <div className="text-3xl font-bold pb-6">filters</div> 
-                            <div className="flex flex-col gap-8">
-                                <div>
-                                    <div className="text-2xl font-bold pb-2">wood type:</div>
-                                    <div className="flex flex-row flex-wrap gap-2">
-                                        <UprightFilterButton filterToToggle="carved" filters={filters} toggleFilter={toggleFilter}>
-                                            carved
-                                        </UprightFilterButton>
-                                        <UprightFilterButton filterToToggle="hybrid" filters={filters} toggleFilter={toggleFilter}>
-                                            hybrid
-                                        </UprightFilterButton>
-                                        <UprightFilterButton filterToToggle="plywood" filters={filters} toggleFilter={toggleFilter}>
-                                            plywood
-                                        </UprightFilterButton>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div>
-                                    </div>
-                                    <div>
-                                        <div className="text-2xl font-bold pb-2">keywords:</div>
-                                        <div className="flex flex-row items-center gap-6 text-2xl font-bold">
-                                            <Input className="" placeholder="enter keyword here" label="keyword" onChange={(e) => setKeyword(e.target.value)} value={keyword} />
-                                            <button onClick={() => handleKeywordAdd(keyword)}>+</button>
-                                        </div>
-                                        {renderKeywords(filters.keywords ?? [], removeKeyword)}
-        
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="text-2xl font-bold pb-2">price range:</div>
-                                    <PriceRangeComponent filters={filters} setFilters={setFilters} />
-                                </div>
-                            </div>  
-                        </div>
+  if (!filters) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="pt-20">
+      <div className="flex flex-row w-full pl-20 pr-40 gap-40 justify-between">
+        <div className="sticky top-0 h-full flex flex-row pt-">
+          <div className="min-w-[20rem]">
+            <div className="flex flex-col items-center">
+              <div className="text-3xl font-bold pb-6">filters</div>
+              <div className="flex flex-col gap-8">
+                <div>
+                  <div className="text-2xl font-bold pb-2">wood type:</div>
+                  <div className="flex flex-row flex-wrap gap-2">
+                    <UprightFilterButton
+                      filterToToggle="carved"
+                      filters={filters}
+                      toggleFilter={toggleFilter}
+                    >
+                      carved
+                    </UprightFilterButton>
+                    <UprightFilterButton
+                      filterToToggle="hybrid"
+                      filters={filters}
+                      toggleFilter={toggleFilter}
+                    >
+                      hybrid
+                    </UprightFilterButton>
+                    <UprightFilterButton
+                      filterToToggle="plywood"
+                      filters={filters}
+                      toggleFilter={toggleFilter}
+                    >
+                      plywood
+                    </UprightFilterButton>
+                  </div>
+                </div>
+                <div>
+                  <div></div>
+                  <div>
+                    <div className="text-2xl font-bold pb-2">keywords:</div>
+                    <div className="flex flex-row items-center gap-6 text-2xl font-bold">
+                      <Input
+                        className=""
+                        placeholder="enter keyword here"
+                        label="keyword"
+                        onChange={(e) => setKeyword(e.target.value)}
+                        value={keyword}
+                      />
+                      <button onClick={() => handleKeywordAdd(keyword)}>
+                        +
+                      </button>
                     </div>
+                    {renderKeywords(filters.keywords ?? [], removeKeyword)}
+                  </div>
                 </div>
-    
-                <div className="pb-40 pr-20">
-                    <div className="text-3xl font-bold pb-6">your matches ({listings.length}):</div>
-                    <UprightListingsGrid listings={listings} />
+                <div>
+                  <div className="text-2xl font-bold pb-2">price range:</div>
+                  <PriceRangeComponent
+                    filters={filters}
+                    setFilters={setFilters}
+                  />
                 </div>
+              </div>
             </div>
-       </div>
-    );
+          </div>
+        </div>
+
+        <div className="pb-40 pr-20">
+          <div className="text-3xl font-bold pb-6">
+            your matches ({listings.length}):
+          </div>
+          <UprightListingsGrid listings={listings} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UprightClientComponent;
--- frontend/src/app/upright/UprightListingsGrid.tsx
+++ frontend/src/app/upright/UprightListingsGrid.tsx
@@ -1,49 +1,68 @@
 "use client";
 import { UprightBassFilters } from "../components/FilterInterfaces";
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
 import UprightBassListing from "./UprightBassListing";
 
 export interface UprightListingsProps {
-    listings: UprightBassListing[];
+  listings: UprightBassListing[];
 }
 
-
-
 const UprightListingsGrid: React.FC<UprightListingsProps> = ({ listings }) => {
-
-    return (
-        <div className="grid grid-cols-4 gap-10">
-            {listings.map((listing, index) => (
-                <div key={index}>
-                    <Image src={listing.imgLink.toString()} width={500} height={500} alt={`Picture of listing ${listing.title}`} />
-                    <a className="text-pastel-red underline" href={listing.listingLink} rel="noopener noreferrer" target="_blank">
-                        {listing.title}
-                    </a>
-                    <div>
-                        {listing.source === "talkbass" ? (
-                            <span>
-                                <span className="font-extrabold">Price:</span> ${listing.price}
-                            </span>
-                        ) : listing.source === "basschat" ? (
-                            <span>
-                                <span className="font-extrabold">Price:</span> £{listing.price}
-                            </span>
-                        ) : (
-                            <span>
-                                <span className="font-extrabold">Price:</span> {listing.price}
-                            </span>
-                        )}
-                    </div>
-                    {listing.location ? (<div><span className="font-extrabold">Location:</span> {listing.location}</div>) : null}
-                    {listing.year ? <div><span className="font-extrabold">Year:</span> {listing.year}</div> : null}
-                    < div > <span className="font-extrabold">{listing.saleStatus}</span></div>
-                </div >
-            ))}
-        </div >
-    )
+  return (
+    <div className="grid grid-cols-4 gap-10">
+      {listings.map((listing, index) => (
+        <div key={index}>
+          <Image
+            src={listing?.imgLink?.toString()}
+            width={500}
+            height={500}
+            alt={`Picture of listing ${listing.title}`}
+          />
+          <a
+            className="text-pastel-red underline"
+            href={listing.listingLink}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {listing.title}
+          </a>
+          <div>
+            {listing.source === "talkbass" ? (
+              <span>
+                <span className="font-extrabold">Price:</span> ${listing.price}
+              </span>
+            ) : listing.source === "basschat" ? (
+              <span>
+                <span className="font-extrabold">Price:</span> £{listing.price}
+              </span>
+            ) : (
+              <span>
+                <span className="font-extrabold">Price:</span> {listing.price}
+              </span>
+            )}
+          </div>
+          {listing.location ? (
+            <div>
+              <span className="font-extrabold">Location:</span>{" "}
+              {listing.location}
+            </div>
+          ) : null}
+          {listing.year ? (
+            <div>
+              <span className="font-extrabold">Year:</span> {listing.year}
+            </div>
+          ) : null}
+          <div>
+            {" "}
+            <span className="font-extrabold">{listing.saleStatus}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default UprightListingsGrid;
